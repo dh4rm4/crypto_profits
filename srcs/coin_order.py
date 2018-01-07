52c18dae210d9c03820223f7d83f660b64d0b4ff
dh4rm4 <dh4rm4@protonmail.com>
2018-01-07 20:22:31 +0100

    refs #notifications: delete print from coin_order

--- srcs/coin_order.py
+++ srcs/coin_order.py
@@ -6,16 +6,6 @@ a specific time and price
 """
 
 
-class col:
-    BLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-
 class coin_order(object):
     """
     Coin object to store coin infos
@@ -43,27 +33,3 @@ class coin_order(object):
         if price_diff > 0:
             self.positif_evol = True
         self.value_earn = price_diff * self.number
-
-    def output_color(self):
-        print (col.BOLD, end='')
-        if self.positif_evol is True:
-            print (col.OKGREEN, end='')
-        else:
-            print (col.FAIL, end='')
-
-    def end_color(self):
-        print (col.ENDC, end='')
-
-    def print_stats(self):
-        print (col.UNDERLINE +
-               col.YELLOW +
-               self.symbol +
-               col.ENDC +
-               ':  ', end='')
-        self.output_color()
-        print (str(round(self.value_earn, 2)) + '$', end='')
-        self.end_color()
-        print ('\t->\t', end='')
-        self.output_color()
-        print (str(round(self.percent_earn, 2)) + '%')
-        self.end_color()
