a00ca8d63b13d3965796740ef3d65075503f67ef
dh4rm4 <dh4rm4@protonmail.com>
2018-01-20 13:10:05 +0100

    refs #integration_cryptoCompareAPI: workin but connection problem

--- srcs/fetch_coins_infos.py
+++ srcs/fetch_coins_infos.py
@@ -9,6 +9,16 @@ import requests
 from traceback import format_exc
 
 
+def get_usd_to_eur_conversion(self, price_usd):
+    """
+    Make the price conversion from USD to EUR
+    via exchange rate from European Central Bank data
+    """
+    currencies_exchange_rate = requests.get('http://api.fixer.io/latest?base=USD')
+    usd_to_eur_rate = currencies_exchange_rate.json()['rates']['EUR']
+    return (price_usd * usd_to_eur_rate)
+
+
 def fetch_coins_from_file():
     """
     Open the file coins.txt.
@@ -43,12 +53,13 @@ class get_live_coin_infos(object):
         coinmarketcap_values = coinmarketcap_infos(self.name)
         coinmarketcap_values.get_values()
 
-        coincap_values = coincap_infos(self.name, self.symbol)
-        coincap_values.get_values()
-        self.get_the_average_price(coinmarketcap_values, coincap_values)
+        cryptoCompare_values = cryptoCompare_infos(self.name, self.symbol)
+        cryptoCompare_values.get_values()
+        self.get_the_average_price(coinmarketcap_values, cryptoCompare_values)
 
-    def get_the_average_price(self, coinmarketcap, coincap_values):
-        self.current_price = (coinmarketcap.current_price + coincap_values.current_price) / 2
+    def get_the_average_price(self, coinmarketcap, cryptoCompare_values):
+        self.current_price = (coinmarketcap.current_price +
+                              cryptoCompare_values.current_price) / 2
 
 
 class coinmarketcap_infos(object):
@@ -83,9 +94,9 @@ class coinmarketcap_infos(object):
         self.current_price = float(infos['price_eur'])
 
 
-class coincap_infos(object):
+class cryptoCompare_infos(object):
     """
-    Get and store live infos from coinmarketcap.com
+    Get and store live infos from cryptoCompare API
     """
     def __init__(self, name, symbol):
         self.name = name
@@ -97,30 +108,15 @@ class coincap_infos(object):
         Get last infos about coin
         """
         try:
-            url = 'http://coincap.io/page/' + self.symbol
+            url = 'https://min-api.cryptocompare.com/data/price?fsym=' \
+                    + self.symbol \
+                    + '&tsyms=EUR'
             rep = requests.get(url)
-            self.store_infos(rep.json())
+            self.store_price(rep.json())
 
         except Exception as err:
-            print ('Error when calling API: ', err)
-            print (format_exc())
-            exit()
+            print ('[-] Error when calling cryptoCompareAPI with: ', self.symbol)
+            # print (format_exc())
 
-    def store_infos(self, infos):
-        """
-        Store price infos in object from coinCap data or
-        make conversion from USD to EUR
-        """
-        if 'price_eur' in infos:
-            self.current_price = float(infos['price_eur'])
-        else:
-            self.current_price = self.get_usd_to_eur_conversion(infos['price_usd'])
-
-    def get_usd_to_eur_conversion(self, price_usd):
-        """
-        Make the price conversion from USD to EUR
-        via exchange rate from European Central Bank data
-        """
-        currencies_exchange_rate = requests.get('http://api.fixer.io/latest?base=USD')
-        usd_to_eur_rate = currencies_exchange_rate.json()['rates']['EUR']
-        return (price_usd * usd_to_eur_rate)
+    def store_price(self, info):
+        self.current_price = float(info['EUR'])
